d08defc9c8ac4d85ae45eaa82da18162a6f3167c
Holden Malinchock <hmalinch@gmu.edu>
2016-09-30 13:31:02 -0400

    Documentatoin
    Just more commenting

--- 13 - Switch/src/Application.java
+++ 13 - Switch/src/Application.java
@@ -4,12 +4,13 @@ public class Application {
  
     
 	public static void main(String[] args) {
- 
+ 	//Declaring the Scanner so that we can read in keystrokes from the user
         Scanner input = new Scanner(System.in);
- 
+ 	//Asking user for a command
         System.out.println("Please enter a command: ");
+	//Getting what the user type and storing it as text
         String text = input.nextLine();
- 
+ 	//Checks text and begins go through the cases comparing if it is equal to one of the cases
         switch (text) {
         case "start":
             System.out.println("Machine started!");
@@ -18,7 +19,7 @@ public class Application {
         case "stop":
             System.out.println("Machine stopped.");
             break;
- 
+ 	//If none of the cases are met then the default will happen
         default:
             System.out.println("Command not recognized");
         }
